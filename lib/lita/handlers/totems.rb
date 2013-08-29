d3654f8d365f6a7024f235abe5718abaef1bf799
Charles Finkel <charles.finkel@gmail.com>
2013-08-28 20:01:59 -0700

    DRY refactoring

--- lib/lita/handlers/totems.rb
+++ lib/lita/handlers/totems.rb
@@ -3,65 +3,45 @@ require "lita"
 module Lita
   module Handlers
     class Totems < Handler
-      TOTEMS_PREFIX_REGEX = /^totems?\s+/.source
-      route(
+      def self.route_regex(action_capture_group)
         %r{
-        #{TOTEMS_PREFIX_REGEX}
-        (add|join|take|queue)\s+
+        ^totems?\s+
+        (#{action_capture_group})\s+
         (?<totem>\w+)
-        }x,
-        :add,
-        help: {
-          'totems add TOTEM' => "Adds yourself to the TOTEM queue, or assigns yourself to the TOTEM if it's unassigned"
-        })
+        }x
+      end
+
+      route(route_regex("add|join|take|queue"), :add,
+            help: {
+                'totems add TOTEM' => "Adds yourself to the TOTEM queue, or assigns yourself to the TOTEM if it's unassigned"
+            })
+
+      route(route_regex("yield|finish|leave|done|complete|remove"), :yield,
+            help: {
+                'totems yield TOTEM' => 'Yields the TOTEM.  If you are in the queue for the totem, leaves the queue.'
+            })
+
+      route(route_regex("create"), :create,
+            help: {
+                'totems create TOTEM' => 'Creates a new totem TOTEM.'
+            })
+
+      route(route_regex("destroy|delete"), :destroy,
+            help: {
+                'totems destroy TOTEM' => 'Destroys totem TOTEM.'
+            })
 
       route(
-        %r{
-        #{TOTEMS_PREFIX_REGEX}
-        (yield|finish|leave|done|complete|remove)\s+
-        (?<totem>\w+)
-        }x,
-        :yield,
-        help: {
-          'totems yield TOTEM' => 'Yields the TOTEM.  If you are in the queue for the totem, leaves the queue.'
-        })
-
-      route(
-        %r{
-        #{TOTEMS_PREFIX_REGEX}
-        (create)\s+
-        (?<totem>\w+)
-        }x,
-        :create,
-        help: {
-          'totems create TOTEM' => 'Creates a new totem TOTEM.'
-        })
-
-      route(
-        %r{
-        #{TOTEMS_PREFIX_REGEX}
-        (destroy|delete)\s+
-        (?<totem>\w+)
-        }x,
-        :destroy,
-        help: {
-          'totems destroy TOTEM' => 'Destroys totem TOTEM.'
-        })
-
-      route(
-        %r{
-        #{TOTEMS_PREFIX_REGEX}
-        (kick)\s+
-        (?<totem>\w+)
+          %r{#{route_regex("kick").source}
         (
           \s+(?<user>\w+)
         )?
         }x,
-        :kick,
-        help: {
-          'totems kick TOTEM'      => 'Kicks the user currently in possession of the TOTEM off.',
-          'totems kick TOTEM USER' => 'Kicks the specified user from the totem.'
-        })
+          :kick,
+          help: {
+              'totems kick TOTEM' => 'Kicks the user currently in possession of the TOTEM off.',
+              'totems kick TOTEM USER' => 'Kicks the specified user from the totem.'
+          })
 
       def destroy(response)
         totem = response[:totem]
